a4c130a9f4d6e78b0223accc08d081edef22902d
Henrique de Carvalho <decarv.henrique@gmail.com>
2023-06-25 17:16:35 -0300

    Update Scoreboard class to add error handling
    Changed timeStamp variable name to timestamp and added file error handling to the class to work effectively inside Docker container.

--- app/src/main/kotlin/galaxyraiders/core/score/Scoreboard.kt
+++ app/src/main/kotlin/galaxyraiders/core/score/Scoreboard.kt
@@ -1,43 +1,61 @@
 package galaxyraiders.core.score
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import galaxyraiders.core.game.Asteroid
 import java.io.File
-import com.fasterxml.jackson.module.kotlin.readValue
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import java.io.FileNotFoundException
 import java.sql.Timestamp
 
 class Scoreboard {
-  private val gameId : Int
+  private val gameId: Int
   private val timestamp: Timestamp
   private var score: Double = 0.0
   private var explodedAsteroidsNumber: Int = 0
+  private val mapper = jacksonObjectMapper()
+  private val file = File("Scoreboard.json")
 
   init {
-    val mapper = jacksonObjectMapper()
-    val file = File("Scoreboard.json")
-    val scoreboards: List<ScoreboardData> = if(file.length() > 0) mapper.readValue(file) else listOf()
+    if (!file.exists()) {
+      file.createNewFile()
+      val scoreboards: List<ScoreboardData> = listOf()
+      mapper.writeValue(File("Scoreboard.json"), scoreboards)
+    }
+    val scoreboards: List<ScoreboardData> = if (file.length() > 0) mapper.readValue(file) else listOf()
     val lastScoreboard: ScoreboardData? = scoreboards.lastOrNull()
     gameId = if (lastScoreboard == null) 1 else lastScoreboard.gameId + 1
     timestamp = Timestamp(System.currentTimeMillis())
   }
 
-  fun computePoints(asteroid : Asteroid) {
+  fun computePoints(asteroid: Asteroid) {
     score += asteroid.mass * asteroid.radius
     explodedAsteroidsNumber += 1
   }
 
-  fun getLeaderboardData() : LeaderboardData {
+  fun exportLeaderboardData(): LeaderboardData {
     return LeaderboardData(this.gameId, this.timestamp, this.score)
   }
 
-  fun save() {
-    val mapper = jacksonObjectMapper()
-    val scoreboards: MutableList<ScoreboardData> = mapper.readValue(File("Scoreboard.json"))
-    scoreboards.add(ScoreboardData(gameId, timestamp, explodedAsteroidsNumber, score))
-    mapper.writeValue(File("Scoreboard.json"), scoreboards)
+  fun getScore(): Double {
+    return this.score
   }
 
-  override fun toString() : String {
+  fun save(): Boolean {
+    return try {
+      val scoreboards: MutableList<ScoreboardData> = if (file.exists() && file.length() > 0) {
+        mapper.readValue(file)
+      } else {
+        mutableListOf()
+      }
+      scoreboards.add(ScoreboardData(gameId, timestamp, explodedAsteroidsNumber, score))
+      mapper.writeValue(File("Scoreboard.json"), scoreboards)
+      true
+    } catch (e: FileNotFoundException) {
+      println("Error while saving scoreboard: ${e.message}")
+      false
+    }
+  }
+
+  override fun toString(): String {
     return "Score: $score"
   }
-
 }
